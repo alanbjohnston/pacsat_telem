351ae349de789badf6b22c1a9dcf50d0e7860ffa
Alan Johnston <alan.b.johnston@gmail.com>
2026-03-08 10:24:06 -0400

    Update telem_main.c add sleep 1 to avoid excessive CPU utilization

--- src/telem_main.c
+++ src/telem_main.c
@@ -211,6 +211,8 @@ int main(int argc, char *argv[]) {
 		if (g_num_of_file_io_errors > MAX_NUMBER_FILE_IO_ERRORS) {
 			printf("ERROR: Too many file io/errors.  Exiting\n");
 			signal_exit(0);
+		sleep(1); // wait 1 second before checking again
+	
 		}
 
 	} /* while (1) */
